b69001bcb8aecaffa132e7dc9b8e61f5f556bf64
Barnali Chowdhury <barnali@dunegpvm07.fnal.gov>
2021-12-08 02:59:07 -0600

    Adding Infrastructure for Coldbox/hd/ (Hor. Drift Coldbox Analysis).

--- duneprototypes/Coldbox/hd/HDColdboxDataInterface_tool.cc
+++ duneprototypes/Coldbox/hd/HDColdboxDataInterface_tool.cc
@@ -1,4 +1,4 @@
-#include "VDColdboxDataInterface.h"
+#include "HDColdboxDataInterface.h"
 
 #include <hdf5.h>
 #include <iostream>
@@ -16,7 +16,8 @@
 #include "dune/DuneObj/DUNEHDF5FileInfo.h"
 #include "dune/HDF5Utils/HDF5Utils.h"
 #include "detdataformats/wib/WIBFrame.hpp"
-#include "dune/Coldbox/vd/ChannelMap/VDColdboxChannelMapService.h"
+#include "dune-raw-data/Services/ChannelMap/PdspChannelMapService.h"
+
 
 
 //The file handle is from the raw::DUNEHDF5FileInfo data product that the source puts into the event. Art's getHandle<type> is usedto retrieve a data product from the event.  
@@ -156,15 +157,16 @@ void readFragmentsForEvent (art::Event &evt)
 
 }
 
-// Keep this here for now. This needs scrutiny regarding whether the input labels are fetching right values.
-VDColdboxDataInterface::VDColdboxDataInterface(fhicl::ParameterSet const& p)
+
+HDColdboxDataInterface::HDColdboxDataInterface(fhicl::ParameterSet const& p)
   : fForceOpen(p.get<bool>("ForceOpen", false)),
     fFileInfoLabel(p.get<std::string>("FileInfoLabel", "daq")),
-    fMaxChan(p.get<int>("MaxChan",1000000)) {
+    fMaxChan(p.get<int>("MaxChan",1000000)), 
+    fDefaultCrate(p.get<unsigned int>("DefaultCrate", 3)) {
 }
 
 
-int VDColdboxDataInterface::retrieveData(art::Event &evt, 
+int HDColdboxDataInterface::retrieveData(art::Event &evt, 
 					 std::string inputLabel, 
 					 std::vector<raw::RawDigit> &raw_digits, 
 					 std::vector<raw::RDTimeStamp> &rd_timestamps,
@@ -173,7 +175,7 @@ int VDColdboxDataInterface::retrieveData(art::Event &evt,
 }
 
 
-int VDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
+int HDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
                                                          std::vector<raw::RawDigit> &raw_digits,
                                                          std::vector<raw::RDTimeStamp> &rd_timestamps,
                                                          std::vector<raw::RDStatus> &rdstatuses,
@@ -185,18 +187,15 @@ int VDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
   const std::string & file_name = infoHandle->GetFileName();
   hid_t file_id = infoHandle->GetHDF5FileHandle();
   
-  std::cout << "HDF5 FileName: " << file_name << std::endl;
-  std::cout << "Top-Level Group Name: " << toplevel_groupname << std::endl;
+  // std::cout << "HDF5 FileName: " << file_name << std::endl;
+  // std::cout << "Top-Level Group Name: " << toplevel_groupname << std::endl;
   
-  //If the fcl file said to force open the file
-  //(i.e. because one is just running DataPrep), then open
-  //but only if we are on a new file -- identified by if the handle
-  //stored in the event is different
+  // If the fcl file said to force open the file (i.e. because one is just running DataPrep), then open
+  // but only if we are on a new file -- identified by if the handle stored in the event is different.
   if (fForceOpen && (file_id != fPrevStoredHandle))
     {
-      std::cout << "Opening" << std::endl;
       fHDFFile = H5Fopen(file_name.data(), H5F_ACC_RDONLY, H5P_DEFAULT);
-    }//If the handle is the same, fHDFFile won't change
+    } // If the handle is the same, fHDFFile won't change
   else if (!fForceOpen)
     {
       fHDFFile = file_id;
@@ -205,16 +204,16 @@ int VDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
   
   hid_t the_group = getGroupFromPath(fHDFFile, toplevel_groupname);
   
-  std::cout << "Retrieving Data for " << apalist.size() << " APA " << std::endl;
+  // std::cout << "Retrieving Data for " << apalist.size() << " APA " << std::endl;
   
   // NOTE: The "apalist" that DataPrep hands to the method is always of size 1.
   // Also "apalist" should technically hand you the current APA No. we are looking at but there is exception.
-  // CAUTION: This is only and only for VDColdBox.The reason is VDColdBox has only one APA/CRU.
+  // CAUTION: This is only and only for HDColdBox.The reason is VDColdBox has only one APA/CRU.
   for (const int & i : apalist)
     {
       int apano = i;
       std::cout << "apano: " << i << std::endl;
-
+      
       getFragmentsForEvent(the_group, raw_digits, rd_timestamps, apano, fMaxChan);
       
       //Currently putting in dummy values for the RD Statuses
@@ -227,29 +226,28 @@ int VDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
 
 
 // get data for a specific label, but only return those raw digits that correspond to APA's on the list
-int VDColdboxDataInterface::retrieveDataAPAListWithLabels(
-    art::Event &evt, 
-    std::string inputLabel, 
-    std::vector<raw::RawDigit> &raw_digits, 
-    std::vector<raw::RDTimeStamp> &rd_timestamps,
-    std::vector<raw::RDStatus> &rdstatuses, 
-    std::vector<int> &apalist) {
+int HDColdboxDataInterface::retrieveDataAPAListWithLabels( art::Event &evt, 
+                                                           std::string inputLabel, 
+                                                           std::vector<raw::RawDigit> &raw_digits, 
+                                                           std::vector<raw::RDTimeStamp> &rd_timestamps,
+                                                           std::vector<raw::RDStatus> &rdstatuses, 
+                                                           std::vector<int> &apalist) 
+{
   return 0;
 }
 
 
 // This is designed to read 1APA/CRU, only for VDColdBox data. The function uses "apano", handed by DataPrep,
 // as an argument.
-void VDColdboxDataInterface::getFragmentsForEvent(
-    hid_t the_group, RawDigits& raw_digits, RDTimeStamps &timestamps,
-    int apano, int maxchan) {
-
+void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& raw_digits, RDTimeStamps &timestamps,
+                                                   int apano, unsigned int maxchan) 
+{
   using namespace dune::HDF5Utils;
   using dunedaq::detdataformats::wib::WIBFrame;
   using dunedaq::detdataformats::wib::WIBHeader;
-
-  art::ServiceHandle<dune::VDColdboxChannelMapService> channelMap;
   
+  art::ServiceHandle<dune::PdspChannelMapService> channelMap;
+
   std::deque<std::string> det_types
     = getMidLevelGroupNames(the_group);
 
@@ -261,13 +259,13 @@ void VDColdboxDataInterface::getFragmentsForEvent(
       std::deque<std::string> apaNames
         = getMidLevelGroupNames(geoGroup);
       
-      std::cout << "Size of apaNames: " << apaNames.size() << std::endl;
-      std::cout << "apaNames[apano]: "  << apaNames[apano-1] << std::endl;
+      // std::cout << "Size of apaNames: " << apaNames.size() << std::endl;
+      // std::cout << "apaNames[apano]: "  << apaNames[apano-1] << std::endl;
       
       // apaNames is a vector whose elements start at [0].
       hid_t linkGroup = getGroupFromPath(geoGroup, apaNames[apano-1]);
-      std::deque<std::string> linkNames
-        = getMidLevelGroupNames(linkGroup);
+      std::deque<std::string> linkNames = getMidLevelGroupNames(linkGroup);
+
       for (const auto & t : linkNames)
         {
           hid_t dataset = H5Dopen(linkGroup, t.data(), H5P_DEFAULT);
@@ -275,18 +273,20 @@ void VDColdboxDataInterface::getFragmentsForEvent(
           if (ds_size <= sizeof(FragmentHeader)) continue; //Too small
           
           std::vector<char> ds_data(ds_size);
-          H5Dread(dataset, H5T_STD_I8LE, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-                  ds_data.data());
+          H5Dread(dataset, H5T_STD_I8LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, ds_data.data());
           H5Dclose(dataset);
           
           //Each fragment is a collection of WIB Frames
           Fragment frag(&ds_data[0], Fragment::BufferAdoptionMode::kReadOnlyMode);
           size_t n_frames = (ds_size - sizeof(FragmentHeader))/sizeof(WIBFrame);
-          std::vector<raw::RawDigit::ADCvector_t> adc_vectors(256);
-          uint32_t slot = 0, fiber = 0;
+	  std::cout << "n_frames : " << n_frames << std::endl;
+	  std::vector<raw::RawDigit::ADCvector_t> adc_vectors(256);
+	  unsigned int slot = 0, fiber = 0;
+
           for (size_t i = 0; i < n_frames; ++i)
             {
-              auto frame = reinterpret_cast<WIBFrame*>(static_cast<uint8_t*>(frag.get_data()) + i*sizeof(WIBFrame));
+	      auto frame = reinterpret_cast<WIBFrame*>(static_cast<uint8_t*>(frag.get_data()) + i*sizeof(WIBFrame));
+	      // std::cout << "frame : " << frame << std::endl;
               for (size_t j = 0; j < adc_vectors.size(); ++j)
                 {
                   adc_vectors[j].push_back(frame->get_channel(j));
@@ -298,21 +298,51 @@ void VDColdboxDataInterface::getFragmentsForEvent(
                   fiber = frame->get_wib_header()->fiber_no;
                 }
             }
-          //std::cout << "slot, fiber: "  << slot << ", " << fiber << std::endl;
+	  // std::cout << "slot, fiber: "  << slot << ", " << fiber << std::endl;
           for (size_t iChan = 0; iChan < 256; ++iChan)
             {
               const raw::RawDigit::ADCvector_t & v_adc = adc_vectors[iChan];
-      	//std::cout << "Channel: " << iChan << " N ticks: " << v_adc.size() << " Timestamp: " << frag.get_trigger_timestamp() << std::endl;
+	      //std::cout << "Channel: " << iChan << " N ticks: " << v_adc.size() << " Timestamp: " << frag.get_trigger_timestamp() << std::endl;
 
-              int offline_chan = channelMap->getOfflChanFromSlotFiberChan(slot, fiber, iChan);
+	      // handle 256 channels on two fibers -- use the channel map that assumes 128 chans per fiber (=FEMB)
+	      // Channels 0-127 are on "fiberloc" 1 and channels 128-255 are on fiberloc 2.
+	      // Use separate variables, for example, "fiberloc" and "chloc" to keep track of the actual channel and fiber and to accommodate future needs.
+	      unsigned int fiberloc = 0;
+	      if (fiber == 1) 
+		{
+		  fiberloc = 1;
+		}
+
+	      else if (fiber == 2)
+		{
+		  fiberloc = 2;
+		}
+	      else
+		{
+		  MF_LOG_WARNING("_process_FELIX_AUX:") << " Fiber number " << (int) fiber << " is expected to be 1 or 2 -- revisit logic";
+		  fiberloc = 1;
+		}
+
+	      unsigned int chloc = iChan;
+	      if (chloc > 127)
+		{
+		  chloc -= 128;
+		  fiberloc++;
+		}
+
+	      //In the channel map call, the crate number is ill-defined for the HD coldbox, as there is only one crate, and the dataprep and event display have room for six.  Pick Default Crate number 3 to send in to the call
+	      unsigned int offline_chan = channelMap->GetOfflineNumberFromDetectorElements(fDefaultCrate, slot, fiberloc, chloc, dune::PdspChannelMapService::kFELIX);
+	      std::cout << "iChan : " << iChan << std::endl;
+	      std::cout << "offline_chan  : " << offline_chan << std::endl;
+
               if (offline_chan < 0) continue;
-	        if (offline_chan > maxchan) continue;
-      	raw::RDTimeStamp rd_ts(frag.get_trigger_timestamp(), offline_chan);
+	      if (offline_chan > maxchan) continue;
+	      raw::RDTimeStamp rd_ts(frag.get_trigger_timestamp(), offline_chan);
               timestamps.push_back(rd_ts);
-      	
+	      
               float median = 0., sigma = 0.;
               getMedianSigma(v_adc, median, sigma);
-      	raw::RawDigit rd(offline_chan, v_adc.size(), v_adc);
+	      raw::RawDigit rd(offline_chan, v_adc.size(), v_adc);
               rd.SetPedestal(median, sigma);
               raw_digits.push_back(rd);
             }
@@ -324,7 +354,7 @@ void VDColdboxDataInterface::getFragmentsForEvent(
 }
 
 
-void VDColdboxDataInterface::getMedianSigma(
+void HDColdboxDataInterface::getMedianSigma(
     const raw::RawDigit::ADCvector_t &v_adc, float &median,
     float &sigma) {
   size_t asiz = v_adc.size();
@@ -357,4 +387,4 @@ void VDColdboxDataInterface::getMedianSigma(
   
 }
 
-DEFINE_ART_CLASS_TOOL(VDColdboxDataInterface)
+DEFINE_ART_CLASS_TOOL(HDColdboxDataInterface)
