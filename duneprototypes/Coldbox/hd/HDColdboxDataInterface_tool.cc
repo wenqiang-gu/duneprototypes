2791042a3c4844e07a40e4e69715122d1674f61d
Barnali Chowdhury <barnali@dunegpvm05.fnal.gov>
2021-12-13 04:17:27 -0600

    Prefix cout statements with HDColdboxDataInterface.

--- duneprototypes/Coldbox/hd/HDColdboxDataInterface_tool.cc
+++ duneprototypes/Coldbox/hd/HDColdboxDataInterface_tool.cc
@@ -20,195 +20,6 @@
 
 
 
-//The file handle is from the raw::DUNEHDF5FileInfo data product that the source puts into the event. Art's getHandle<type> is usedto retrieve a data product from the event.  
-// The idea is hand this function an art event and it will return you APA/CRU info FOR THE VDColdbox.
-
-/*
-void readFragmentsForEvent (art::Event &evt)
-{
-
-  using namespace dune::HDF5Utils;
-  auto infoHandle = evt.getHandle <raw::DUNEHDF5FileInfo> ("daq");
-  if (fDebugLevel > 0)
-    {
-      std::cout << "Got infos? " << infoHandle << std::endl;
-    }  
-  const std::string & toplevel_groupname = infoHandle->GetEventGroupName();
-  const std::string & file_name = infoHandle->GetFileName();
-  hid_t file_id = infoHandle->GetHDF5FileHandle();
-
-  if (fDebugLevel > 0)
-    {
-      std::cout << "Top-Level Group Name: " << toplevel_groupname << std::endl;
-      std::cout << "HDF5 FileName: " << file_name << std::endl;
-    }
-
-  // now look inside those "Top-Level Group Name" for "Detector type".
-  hid_t requestedGroup = getGroupFromPath(file_id, toplevel_groupname);
-
-  std::deque<std::string> detectorTypeNames = getMidLevelGroupNames(requestedGroup);
-  
-  for (auto& detectorTypeName : detectorTypeNames)
-    {
-      if (detectorTypeName == "TPC" && detectorTypeName != "TriggerRecordHeader")
-	{
-	  if (fDebugLevel > 0)
-	    {
-	      std::cout << "  Detector type: " << detectorTypeName << std::endl;
-	    }
-	  std::string geoPath = toplevel_groupname + "/" + detectorTypeName;
-	  hid_t geoGroup = getGroupFromPath(file_id,geoPath);
-	  std::deque<std::string> apaNames = getMidLevelGroupNames(geoGroup);
-	  
-	  // loop over APAs
-	  for (auto& apaName : apaNames)
-	    {
-	      std::string apaGroupPath = geoPath + "/" + apaName;
-	      if (fDebugLevel > 0)
-		{
-		  std::cout << "     Geo path: " << apaGroupPath << std::endl;
-		}
-	      hid_t linkGroup = getGroupFromPath(file_id,apaGroupPath);
-	      std::deque<std::string> linkNames = getMidLevelGroupNames(linkGroup);
-	      
-	      // loop over Links
-	      for (auto& linkName : linkNames)
-		{
-		  std::string dataSetPath = apaGroupPath + "/" + linkName;
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "      Data Set Path: " << dataSetPath << std::endl;
-		    }
-		  hid_t datasetid = H5Dopen(linkGroup,linkName.data(),H5P_DEFAULT);
-		  hsize_t ds_size = H5Dget_storage_size(datasetid);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "      Data Set Size (bytes): " << ds_size << std::endl;
-		    }		  
-		  if (ds_size < 80) continue;
-		  
-		  size_t narray = ds_size / sizeof(char);
-		  size_t rdr = ds_size % sizeof(char);
-		  if (rdr > 0 || narray == 0) narray++;
-		  char *ds_data = new char[narray];
-		  //herr_t ecode = 
-		  H5Dread(datasetid, H5T_STD_I8LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, ds_data);
-		  int firstbyte = ds_data[0];
-		  firstbyte &= 0xFF;
-		  int lastbyte = ds_data[narray-1];
-		  lastbyte &= 0xFF;
-		  
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << std::hex << "      Retrieved data: ecode: " << ecode << "  first byte: " << firstbyte
-				<< " last byte: " << lastbyte  << std::dec << std::endl;
-		    }
-
-		  int magic_word = 0;
-		  memcpy(&magic_word,&ds_data[0],4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Magic word: 0x" << std::hex << magic_word << std::dec << std::endl;
-		    }		  
-		  int version = 0;
-		  memcpy(&version, &ds_data[4],4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Version: " << std::dec << version << std::dec << std::endl;
-		    }		  
-		  uint64_t fragsize=0;
-		  memcpy(&fragsize, &ds_data[8],8);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Frag Size: " << std::dec << fragsize << std::dec << std::endl;
-		    }		  
-		  uint64_t trignum=0;
-		  memcpy(&trignum, &ds_data[16],8);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Trig Num: " << std::dec << trignum << std::dec << std::endl;
-		    }		  
-		  uint64_t trig_timestamp=0;
-		  memcpy(&trig_timestamp, &ds_data[24],8);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Trig Timestamp: " << std::dec << trig_timestamp << std::dec << std::endl;
-		    }		  
-		  uint64_t windowbeg=0;
-		  memcpy(&windowbeg, &ds_data[32],8);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Window Begin:   " << std::dec << windowbeg << std::dec << std::endl;
-		    }		  
-		  uint64_t windowend=0;
-		  memcpy(&windowend, &ds_data[40],8);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Window End:     " << std::dec << windowend << std::dec << std::endl;
-		    }		  
-		  int runno=0;
-		  memcpy(&runno, &ds_data[48], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Run Number: " << std::dec << runno << std::endl;
-		    }		  
-		  int errbits=0;
-		  memcpy(&errbits, &ds_data[52], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Error bits: " << std::dec << errbits << std::endl;
-		    }		  
-		  int fragtype=0;
-		  memcpy(&fragtype, &ds_data[56], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Fragment type: " << std::dec << fragtype << std::endl;
-		    }		  
-		  int fragpadding=0;
-		  memcpy(&fragpadding, &ds_data[60], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   Fragment padding: " << std::dec << fragpadding << std::endl;
-		    }		  
-		  int geoidversion=0;
-		  memcpy(&geoidversion, &ds_data[64], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   GeoID version: " << std::dec << geoidversion << std::endl;
-		    }		  
-		  unsigned short geoidtype;
-		  memcpy(&geoidtype, &ds_data[70], 1);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   GeoID type: " << geoidtype << std::endl;
-		    }		  
-		  unsigned short geoidregion=0;
-		  memcpy(&geoidregion, &ds_data[71], 1);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   GeoID region: " << std::dec << geoidregion << std::endl;
-		    }		  
-		  int geoidelement=0;
-		  memcpy(&geoidelement, &ds_data[72], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   GeoID element: " << std::dec << geoidelement << std::endl;
-		    }		  
-		  int geoidpadding=0;
-		  memcpy(&geoidpadding, &ds_data[76], 4);
-		  if (fDebugLevel > 0)
-		    {
-		      std::cout << "   GeoID padding: " << std::dec << geoidpadding << std::endl;
-		    }		  
-		  delete[] ds_data;  // free up memory
-
-		} 
-	    }
-	}
-    }
-}
-*/
-
 HDColdboxDataInterface::HDColdboxDataInterface(fhicl::ParameterSet const& p)
   : fForceOpen(p.get<bool>("ForceOpen", false)),
     fFileInfoLabel(p.get<std::string>("FileInfoLabel", "daq")),
@@ -242,8 +53,8 @@ int HDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
   
   if (fDebugLevel > 0)
     {
-      std::cout << "HDF5 FileName: " << file_name << std::endl;
-      std::cout << "Top-Level Group Name: " << toplevel_groupname << std::endl;
+      std::cout << "HDColdboxDataInterface : " << "HDF5 FileName: " << file_name << std::endl;
+      std::cout << "HDColdboxDataInterface :" << "Top-Level Group Name: " << toplevel_groupname << std::endl;
     }
 
   // If the fcl file said to force open the file (i.e. because one is just running DataPrep), then open
@@ -262,7 +73,7 @@ int HDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
   
   if (fDebugLevel > 0)
     {
-      std::cout << "Retrieving Data for " << apalist.size() << " APAs " << std::endl;
+      std::cout << "HDColdboxDataInterface : " <<  "Retrieving Data for " << apalist.size() << " APAs " << std::endl;
     }
 
   // NOTE: The "apalist" that DataPrep hands to the method is always of size 1.
@@ -273,7 +84,7 @@ int HDColdboxDataInterface::retrieveDataForSpecifiedAPAs(art::Event &evt,
       int apano = i;
       if (fDebugLevel > 0)
 	{
-	  std::cout << "apano: " << i << std::endl;
+	  std::cout << "HDColdboxDataInterface :" << "apano: " << i << std::endl;
 	}
  
       getFragmentsForEvent(the_group, raw_digits, rd_timestamps, apano, fMaxChan);
@@ -318,7 +129,7 @@ void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& ra
       if (det != "TPC") continue;
       if (fDebugLevel > 0)
 	{
-	  std::cout << "  Detector type:  " << det << std::endl;
+	  std::cout << "HDColdboxDataInterface :"  << "Detector type:  " << det << std::endl;
 	}
       hid_t geoGroup = getGroupFromPath(the_group, det);
       std::deque<std::string> apaNames
@@ -326,8 +137,8 @@ void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& ra
       
       if (fDebugLevel > 0)
 	{
-	  std::cout << "Size of apaNames: " << apaNames.size() << std::endl;
-	  std::cout << "apaNames[apano]: "  << apaNames[apano-1] << std::endl;
+	  std::cout << "HDColdboxDataInterface :" << "Size of apaNames: " << apaNames.size() << std::endl;
+	  std::cout << "HDColdboxDataInterface :" << "apaNames[apano]: "  << apaNames[apano-1] << std::endl;
 	}
       // apaNames is a vector whose elements start at [0].
       hid_t linkGroup = getGroupFromPath(geoGroup, apaNames[apano-1]);
@@ -348,7 +159,7 @@ void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& ra
           size_t n_frames = (ds_size - sizeof(FragmentHeader))/sizeof(WIBFrame);
 	  if (fDebugLevel > 0)
 	    {
-	      std::cout << "n_frames : " << n_frames << std::endl;
+	      std::cout << "HDColdboxDataInterface :" << "n_frames : " << n_frames << std::endl;
 	    }
 	  std::vector<raw::RawDigit::ADCvector_t> adc_vectors(256);
 	  unsigned int slot = 0, fiber = 0;
@@ -358,7 +169,7 @@ void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& ra
 	      auto frame = reinterpret_cast<WIBFrame*>(static_cast<uint8_t*>(frag.get_data()) + i*sizeof(WIBFrame));
 	      if (fDebugLevel > 0)
 		{
-		  std::cout << "frame : " << frame << std::endl;
+		  std::cout << "HDColdboxDataInterface :" << "frame : " << frame << std::endl;
 		}
               for (size_t j = 0; j < adc_vectors.size(); ++j)
                 {
@@ -373,14 +184,14 @@ void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& ra
             }
 	  if (fDebugLevel > 0)
 	    {
-	      std::cout << "slot, fiber: "  << slot << ", " << fiber << std::endl;
+	      std::cout << "HDColdboxDataInterface :" << "slot, fiber: "  << slot << ", " << fiber << std::endl;
 	    }
           for (size_t iChan = 0; iChan < 256; ++iChan)
             {
               const raw::RawDigit::ADCvector_t & v_adc = adc_vectors[iChan];
 	      if (fDebugLevel > 0)
 		{
-		  std::cout << "Channel: " << iChan << " N ticks: " << v_adc.size() << " Timestamp: " << frag.get_trigger_timestamp() << std::endl;
+		  std::cout << "HDColdboxDataInterface : " << "Channel: " << iChan << " N ticks: " << v_adc.size() << " Timestamp: " << frag.get_trigger_timestamp() << std::endl;
 		}
 	      // handle 256 channels on two fibers -- use the channel map that assumes 128 chans per fiber (=FEMB)
 	      // Channels 0-127 are on "fiberloc" 1 and channels 128-255 are on fiberloc 2.
@@ -412,8 +223,8 @@ void HDColdboxDataInterface::getFragmentsForEvent(hid_t the_group, RawDigits& ra
 	      unsigned int offline_chan = channelMap->GetOfflineNumberFromDetectorElements(fDefaultCrate, slot, fiberloc, chloc, dune::PdspChannelMapService::kFELIX);
 	      if (fDebugLevel > 0)
 		{
-		  std::cout << "iChan : " << iChan << std::endl;
-		  std::cout << "offline_chan  : " << offline_chan << std::endl;
+		  std::cout << "HDColdboxDataInterface : " << "iChan : " << iChan << std::endl;
+		  std::cout << "HDColdboxDataInterface : " << "offline_chan  : " << offline_chan << std::endl;
 		}
               if (offline_chan < 0) continue;
 	      if (offline_chan > maxchan) continue;
