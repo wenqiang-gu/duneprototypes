742a8b316637a6f69c8990b380ad4609f4718eaa
ligerlac <lino.oscar.gerlach@cern.ch>
2022-05-27 07:55:02 -0500

    rewrote the module for significant speed-up and cleaner code. still contains some debug messages

--- duneprototypes/Protodune/singlephase/DataUtils/ProtoDUNEUnstableHVFilter_module.cc
+++ duneprototypes/Protodune/singlephase/DataUtils/ProtoDUNEUnstableHVFilter_module.cc
@@ -1,11 +1,12 @@
 ////////////////////////////////////////////////////////////////////////
 //
-// ProtoDUNEUnstableHVFilter class
+// ProtoDUNEUnstableHV:Filter class
 // 
-// author: Owen Goodwin
-// email: owen.goodwin@manchester.ac.uk
+// authors: Owen Goodwin, Lino Gerlach
+// emails: owen.goodwin@manchester.ac.uk, lino.oscar.gerlach@cern.ch
 //
-// - A filter to reject events between given start and end times of unstable HV periods. Using raw decoder timestamp.
+// - A filter to reject events between given start and end times of unstable HV periods.
+//   Using raw decoder timestamp.
 // 
 //    - All dates and times should be in unix time (UTC)
 //
@@ -19,6 +20,7 @@ extern "C" {
 #include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <typeinfo>
 // ROOT
 #include "TMath.h"
 #include "TTimeStamp.h"
@@ -40,186 +42,148 @@ extern "C" {
 
 #include "lardataobj/RawData/RDTimeStamp.h"
 #include "dunecore/DuneObj/ProtoDUNETimeStamp.h"
-///filters for events, etc
-namespace filter {
-        class ProtoDUNEUnstableHVFilter : public art::EDFilter  {
-        public:
-                explicit ProtoDUNEUnstableHVFilter(fhicl::ParameterSet const& ); 
-                virtual ~ProtoDUNEUnstableHVFilter();      
-                uint64_t GetRawDecoderInfo(art::Event & e);
-                bool filter(art::Event& evt);
-                void beginJob();
-                void endJob();
-                void reconfigure(fhicl::ParameterSet const& p);
-        private:
 
-                std::vector<std::pair<UInt_t,UInt_t>> fTimeRanges; 
-                UInt_t fTimeRangeLow;
-                UInt_t fTimeRangeHigh;
-                bool fDebug;
-                long long RDTSTime;
-                double RDTSTimeSec;
-                int RDTSTrigger;
-                art::Handle< std::vector<raw::RDTimeStamp> > RDTimeStampHandle;
+#include <chrono>
 
+namespace ProtoDUNEUnstableHV{
 
-                TH1D* fSelectedEvents;
-                TH1D* fTotalEvents;
+class UnstablePeriod {
+    public:
+        void setTimePair(std::pair<UInt_t,UInt_t> timePair);
+        bool containsTimeStamp(const TTimeStamp *ts);
+    private:
+        void checkInputValid(UInt_t begin, UInt_t end);
+        TTimeStamp beginTs;
+        TTimeStamp endTs;
+};
+class UnstablePeriods {
+    public:
+        void setTimePairs(std::vector <std::pair<UInt_t,UInt_t >> timePairs);
+        bool containsTimeStamp(const TTimeStamp *ts);
+    private:
+        UnstablePeriod *subPeriods;
+        int n_periods;
+};
+class DecoderHandler {
+    public:
+        double getTimeStamp(art::Event & e);
+    private:
+        art::InputTag iTag = art::InputTag("timingrawdecoder", "daq");
+        art::Handle< std::vector<raw::RDTimeStamp> > handle;
+        void setHandle(art::Event& evt);
+        raw::RDTimeStamp getRawDigitTimeStamp(art::Event& evt);
+        double getSecsFrom20nsTicks(uint64_t rawTime);
+};
+class Filter : public art::EDFilter  {
+    public:
+        explicit Filter(fhicl::ParameterSet const& );
+        virtual ~Filter();
+        bool filter(art::Event& evt);
+        void beginJob();
+        void endJob();
+        void reconfigure(fhicl::ParameterSet const& p);
+    private:
+        UnstablePeriods *unstablePeriods = new UnstablePeriods;
+        DecoderHandler *decoderHandler = new DecoderHandler;
+        bool keepEvent(art::Event& evt);
+        bool fDebug;
+        TH1D* fSelectedEvents;
+        TH1D* fTotalEvents;
+};
 
-
-        }; //class ProtoDUNEUnstableHVFilter
+void UnstablePeriod::setTimePair(std::pair<UInt_t,UInt_t> timePair){
+    checkInputValid(timePair.first, timePair.second);
+    beginTs = TTimeStamp(timePair.first);
+    endTs = TTimeStamp(timePair.second);
 }
-void filter::ProtoDUNEUnstableHVFilter::beginJob() { 
 
+bool UnstablePeriod::containsTimeStamp(const TTimeStamp *ts){
+    return ts->GetSec() > beginTs.GetSec() && ts->GetSec() < endTs.GetSec();
+}
+
+void UnstablePeriod::checkInputValid(UInt_t begin, UInt_t end){
+    if (begin<1000000000 || end < 1000000000 || begin>end){
+        throw std::logic_error("Unstable period not valid!");
+    }
+}
+
+void UnstablePeriods::setTimePairs(std::vector <std::pair<UInt_t,UInt_t >> timePairs){
+    n_periods = timePairs.size();
+    subPeriods = new UnstablePeriod[n_periods];
+    for (int n=0; n<n_periods; n++){
+        subPeriods[n].setTimePair(timePairs.at(n));
+    }
+}
+
+bool UnstablePeriods::containsTimeStamp(const TTimeStamp *ts){
+    for (int n=0; n<n_periods; n++){
+        if (subPeriods[n].containsTimeStamp(ts)) return true;
+    }
+    return false;
+}
+
+void DecoderHandler::setHandle(art::Event & e) {
+    handle = e.getHandle< std::vector<raw::RDTimeStamp> >(iTag);
+    MF_LOG_INFO("BeamEvent") << "Handle valid? " << handle.isValid() << "\n";
+}
+
+raw::RDTimeStamp DecoderHandler::getRawDigitTimeStamp(art::Event & e) {
+    setHandle(e);
+    for (auto const & rawTs : *handle){
+        return rawTs;
+    }return -1;
+}
+
+double DecoderHandler::getSecsFrom20nsTicks(uint64_t raw20nsTicks){
+    long long ticks = (raw20nsTicks * 2) / (int)(TMath::Power(10,8));
+    ticks = ticks * (int)(TMath::Power(10,8)) / 2;
+    double seconds = 20.e-9 * ticks;
+    return seconds;
+}
+
+double DecoderHandler::getTimeStamp(art::Event & e) {
+    MF_LOG_INFO("BeamEvent") << "Getting Raw Decoder Info" << "\n";
+    raw::RDTimeStamp rDtS = getRawDigitTimeStamp(e);
+    MF_LOG_INFO("BeamEvent") << "Trigger: " << rDtS.GetFlags() << "\n";
+    uint64_t raw20nsTicks = rDtS.GetTimeStamp();
+    return getSecsFrom20nsTicks(raw20nsTicks);
+}
+
+void Filter::beginJob() {
     art::ServiceHandle<art::TFileService> tfs;
     fSelectedEvents = tfs->make<TH1D>("fSelectedEvents", "Number of Selected Events", 3, 0, 3); //counts the number of selected events 
     fTotalEvents = tfs->make<TH1D>("fTotalEvents", "Total Events", 3, 0, 3); //counts the initial number of events in the unfiltered root input file
 }
-void filter::ProtoDUNEUnstableHVFilter::endJob() { }
 
+void Filter::endJob() { }
 
-void filter::ProtoDUNEUnstableHVFilter::reconfigure(fhicl::ParameterSet const& p) {
-        fTimeRanges  = p.get<std::vector <std::pair<UInt_t,UInt_t >>>("TimeRanges");
-        fDebug = p.get<int>("Debug");
-
-}  
-
-
-filter::ProtoDUNEUnstableHVFilter::ProtoDUNEUnstableHVFilter(fhicl::ParameterSet const& pset)
-: EDFilter(pset) {
-        this->reconfigure(pset);
-}
-filter::ProtoDUNEUnstableHVFilter::~ProtoDUNEUnstableHVFilter() { }
-
-
-uint64_t filter::ProtoDUNEUnstableHVFilter::GetRawDecoderInfo(art::Event & e){
-    MF_LOG_INFO("BeamEvent") << "\n";
-    MF_LOG_INFO("BeamEvent") << "Getting Raw Decoder Info" << "\n";
-
-    art::InputTag itag1("timingrawdecoder","daq");
-    RDTimeStampHandle = e.getHandle< std::vector<raw::RDTimeStamp> >(itag1);
-    MF_LOG_INFO("BeamEvent") << "RDTS valid? " << RDTimeStampHandle.isValid() << "\n";
-    for (auto const & RDTS : *RDTimeStampHandle){
-        MF_LOG_INFO("BeamEvent") << "High: " << RDTS.GetTimeStamp_High() << "\n";
-        MF_LOG_INFO("BeamEvent") << "Low: " << RDTS.GetTimeStamp_Low() << "\n";
-
-        uint64_t high = RDTS.GetTimeStamp_High();
-        uint64_t low  = RDTS.GetTimeStamp_Low();
-
-        high = high << 32;
-        uint64_t joined = (high | low);
-
-        MF_LOG_INFO("BeamEvent") << "Raw Decoder Timestamp: " << joined << "\n";
-
-        RDTSTime = joined;
-        RDTSTrigger = RDTS.GetFlags();
-        MF_LOG_INFO("BeamEvent") << "Trigger: " << RDTSTrigger << "\n";
-
-        //Separates seconds portion of the ticks 
-        //From the nanoseconds
-        long long RDTSTickSec = (RDTSTime * 2) / (int)(TMath::Power(10,8));
-        RDTSTickSec = RDTSTickSec * (int)(TMath::Power(10,8)) / 2;
-        //long long RDTSTickNano = RDTSTime - RDTSTickSec;
-
-        //Units are 20 nanoseconds ticks
-        RDTSTimeSec  = 20.e-9 * RDTSTickSec;
-        //RDTSTimeNano = 20.    * RDTSTickNano;
-
-
-  }
-  return RDTSTimeSec;
+void Filter::reconfigure(fhicl::ParameterSet const& p) {
+    unstablePeriods->setTimePairs(p.get<std::vector<std::pair<UInt_t,UInt_t>>>("TimeRanges"));
+    fDebug = p.get<int>("Debug");
 }
 
-bool filter::ProtoDUNEUnstableHVFilter::filter(art::Event &evt) {   
+Filter::Filter(fhicl::ParameterSet const& pset) : EDFilter(pset) {
+    this->reconfigure(pset);
+}
 
+Filter::~Filter() { }
 
-        fTotalEvents->Fill(1);
+bool Filter::keepEvent(art::Event &evt) {
+    if (!evt.isRealData()) return true;
+    TTimeStamp *evtTTS = new TTimeStamp(decoderHandler->getTimeStamp(evt));
+    return !unstablePeriods->containsTimeStamp(evtTTS);
+}
 
-
-
-        if(!evt.isRealData()){
-            fSelectedEvents->Fill(1); 
-            return true;   //Filter is designed for Data only. Don't want to filter on MC
-            }
-
-
-
-        const std::string myname = "ProtoDUNEUnstableHVFilter::filter: ";
-        bool keep = true;
-        TTimeStamp * evtTTS;
-        evtTTS = new TTimeStamp(GetRawDecoderInfo(evt));
-        // if (evtTime.timeHigh() == 0) { evtTTS = new TTimeStamp(evtTime.timeLow()); }
-        // else { evtTTS = new TTimeStamp(evtTime.timeHigh(), evtTime.timeLow()); }
-        if (fDebug) std::cout << "Event time:  " << evtTTS -> AsString() << std::endl;
-        // Requested time range lower end
-
-        for (auto TimeRange : fTimeRanges){ //loop through unstable hv time ranges
-
-            fTimeRangeLow=TimeRange.first;
-            fTimeRangeHigh=TimeRange.second;
-
-
-                        // Check that input time is in correct format
-            
-            
-            // Check that input times are in correct format
-            if (fTimeRangeHigh < 1000000000 || fTimeRangeLow < 1000000000 ) {
-                    std::cout << "Warning: please provide time in POSIX foramt, event time "
-                              << "filter returning false." << std::endl; 
-                    return false;
-
-            }
-
-
-
-            if (fTimeRangeHigh < fTimeRangeLow ) {
-                    std::cout << "Warning: Lower limit bigger than lower limit "
-                              << "filter returning false." << std::endl; 
-                    return false;
-
-            }
-            //no idea what this is supposed to be doing!! will reject any times before 10am
-            // 
-            // if (fTimeRangeHigh > 0 && fTimeRangeHigh < 100000) {
-            //         std::cout << "Warning: please provide time in format HHMMSS, event time "
-            //                   << "filter returning false.2" << std::endl; 
-            //         return false;
-            // }
-            // if (fTimeRangeLow > 0 && fTimeRangeLow < 100000) {
-            //         std::cout << "Warning: please provide time in format HHMMSS, event time "
-            //                   << "filter returning false.3" << std::endl; 
-            //         return false;
-            // }
-            // Event time
-            //art::Timestamp evtTime = evt.time();
-
-
-                // all the checking he dies then ask if its in the correct range
-
-            TTimeStamp * ttsLow(nullptr); 
-            
-        
-            ttsLow = new TTimeStamp(fTimeRangeLow); 
-            // Requested time range higher end
-            TTimeStamp * ttsHigh(nullptr);
-
-            ttsHigh = new TTimeStamp(fTimeRangeHigh); 
-  
-            // Filter decision
-            if(fDebug){
-                std::cout << "Lower Limit:  " << ttsLow -> AsString() << std::endl;
-                std::cout << "Upper Limit:  " << ttsHigh -> AsString() << std::endl;
-            }
-    
-                
-            if (evtTTS -> GetSec() > ttsLow -> GetSec() && 
-                evtTTS -> GetSec() < ttsHigh -> GetSec()) { keep=false; }
-                    
-
+bool Filter::filter(art::Event &evt) {
+    std::cout<<"filter()"<<std::endl;
+    bool keep = false;
+    fTotalEvents->Fill(1);
+    if (keepEvent(evt)) {
+        fSelectedEvents->Fill(1);
+        keep = true;
     }
-    if ( fDebug ) std::cout << myname << (keep ? "Keep" : "Reject") << "ing event." << std::endl;
-    if (keep==true) fSelectedEvents->Fill(1); //count total events
-    //keep=false; //for testing
-    return keep;    //need this to stop the get to end error make sure to check this is working as intended
+    MF_LOG_INFO("BeamEvent") << "GypsyTest: id=" << evt.event() << ", keep=" << keep << "\n";
+    return keep;
 }
- DEFINE_ART_MODULE(filter::ProtoDUNEUnstableHVFilter) 
+}
+DEFINE_ART_MODULE(ProtoDUNEUnstableHV::Filter)
