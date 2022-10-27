56e0a5c13a9cb92448a8c2b67d5ab47e728527a7
Jake Calcutt <calcuttj@msu.edu>
2022-10-27 09:00:28 -0700

    Revert "Accommodate breaking changes in geometry"

--- duneprototypes/Protodune/singlephase/DetectorServices/Providers/DetectorPropertiesProtoDUNEsp.cxx
+++ duneprototypes/Protodune/singlephase/DetectorServices/Providers/DetectorPropertiesProtoDUNEsp.cxx
@@ -555,7 +555,7 @@ namespace spdp{
 
         for(int plane = 0; plane < nplane; ++plane) {
           const geo::PlaneGeo& pgeom = tpcgeom.Plane(plane);
-          auto const xyz = tpcgeom.Plane(0).GetCenter();
+          const double* xyz = tpcgeom.PlaneLocation(0);
           x_ticks_offsets[cstat][tpc][plane] = -xyz[0]/(dir * x_ticks_coefficient) + triggerOffset;
 
           // Add view dependent offset
