88d487729cb814a3773a0b5815d9d3776f0feb2c
David Adams <dladams@bnl.gov>
2023-07-17 16:44:44 +0000

    Add tool PdhdChannelRanges with test. #26

---
+++ duneprototypes/Protodune/hd/Tool/test/test_PdhdChannelRanges.cxx
@@ -0,0 +1,228 @@
+// test_PdhdChannelRanges.cxx
+//
+// David Adams
+// July 2018
+//
+// Test PdhdChannelRanges.
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "dunecore/ArtSupport/DuneToolManager.h"
+#include "dunecore/DuneInterface/Tool/IndexRangeTool.h"
+#include "TH1F.h"
+
+#undef NDEBUG
+#include <cassert>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::istringstream;
+using std::ostringstream;
+using std::setw;
+using std::setfill;
+using std::vector;
+using fhicl::ParameterSet;
+using Index = unsigned int;
+using IndexVector = std::vector<Index>;
+
+//**********************************************************************
+
+int test_PdhdChannelRanges(bool useExistingFcl =false, int show =1) {
+  const string myname = "test_PdhdChannelRanges: ";
+#ifdef NDEBUG
+  cout << myname << "NDEBUG must be off." << endl;
+  abort();
+#endif
+  string line = "-----------------------------";
+
+  cout << myname << line << endl;
+  string fclfile = "test_PdhdChannelRanges.fcl";
+  if ( ! useExistingFcl ) {
+    cout << myname << "Creating top-level FCL." << endl;
+    ofstream fout(fclfile.c_str());
+    fout << "tools: {" << endl;
+    fout << "  mytool: {" << endl;
+    fout << "    tool_type: PdhdChannelRanges" << endl;
+    fout << "    LogLevel: 2" << endl;
+    fout << "    ExtraRanges: \"\"" << endl;
+    fout << "  }" << endl;
+    fout << "}" << endl;
+    fout.close();
+  } else {
+    cout << myname << "Using existing top-level FCL." << endl;
+  }
+
+  Index napa = 4;
+
+  cout << myname << line << endl;
+  cout << myname << "Fetching tool manager." << endl;
+  DuneToolManager* ptm = DuneToolManager::instance(fclfile);
+  assert ( ptm != nullptr );
+  DuneToolManager& tm = *ptm;
+  tm.print();
+  assert( tm.toolNames().size() >= 1 );
+
+  cout << myname << line << endl;
+  cout << myname << "Fetching tool." << endl;
+  auto irt = tm.getPrivate<IndexRangeTool>("mytool");
+  assert( irt != nullptr );
+
+  cout << myname << line << endl;
+  cout << myname << "Set TPS-APA mappings." << endl;
+  vector<bool> isNorthTps = {false, true, false, true};
+  vector<bool> isSouthTps = {true, false, true, false};
+  vector<Index> tpsToApa = {1, 3, 2, 4};
+  vector<Index> apaToTps = {99, 0, 2, 1, 3};
+  Index w = 8;
+  cout << myname << setw(w) << "TPS" << setw(w) << "APA" << endl;
+  for ( Index itps=0; itps<napa; ++itps ) {
+    Index iapa = tpsToApa[itps];
+    cout << myname << setw(w) << itps << setw(w) << iapa << endl;
+    assert( itps < napa );
+    assert( iapa <= napa );
+    assert( apaToTps[iapa] == itps );
+  }
+
+  cout << myname << line << endl;
+  cout << myname << "Fetching TPC ranges." << endl;
+  int nbad = 0;
+  vector<string> namSufs = {   "",   "u",   "v",   "z",  "c",   "x",    "i"};
+  vector<string> namPres(napa+1, "tpp");
+  namPres[0] = "tps";
+  vector<Index> firstTpsZChan(napa+1, 0);
+  vector<Index> firstTpsCChan(napa+1, 0);
+  for ( Index inam=0; inam<namPres.size(); ++inam ) {
+    string svie = namSufs[inam];
+    bool isZ = svie == "z";
+    bool isC = svie == "c";
+    for ( Index itps=0; itps<napa; ++itps ) {
+      string stps = std::to_string(itps);
+      string nam = namPres[inam] + stps + svie;
+      IndexRange ir = irt->get(nam);
+      if ( ! ir.isValid() ) {
+        cout << myname << "Invalid range: " << nam << endl;
+        ++nbad;
+      } else {
+        cout << myname << setw(10) << ir.name << setw(20) << ir.rangeString()
+             << " " << ir.label();
+        for ( Index ilab=1; ilab<ir.labels.size(); ++ilab ) cout << ", " << ir.label(ilab);
+        cout << endl;
+        assert( ir.name == nam );
+      }
+      if ( isZ ) firstTpsZChan[itps] = ir.first();
+      if ( isC ) firstTpsCChan[itps] = ir.first();
+    }
+  }
+  assert( nbad == 0 );
+
+  cout << myname << line << endl;
+  cout << myname << "Fetching APA ranges." << endl;
+  nbad = 0;
+  vector<string> namPresApa(napa+1, "apa");
+  for ( Index inam=0; inam<namPres.size(); ++inam ) {
+    for ( Index iapa=1; iapa<=napa; ++iapa ) {
+      string stps = std::to_string(iapa);
+      string nam = namPresApa[inam] + stps + namSufs[inam];
+      IndexRange ir = irt->get(nam);
+      if ( ! ir.isValid() ) {
+        cout << myname << "Invalid range: " << nam << endl;
+        ++nbad;
+      } else {
+        cout << myname << setw(10) << ir.name << setw(20) << ir.rangeString()
+             << " " << ir.label();
+        for ( Index ilab=1; ilab<ir.labels.size(); ++ilab ) cout << ", " << ir.label(ilab);
+        cout << endl;
+        assert( ir.name == nam );
+      }
+    }
+  }
+  assert( nbad == 0 );
+
+  cout << myname << line << endl;
+  cout << myname << "Check left/right." << endl;
+  for ( Index itps=0; itps<napa; ++itps ) {
+    Index zc = firstTpsZChan[itps];
+    Index cc = firstTpsCChan[itps];
+    cout << myname << "TPS " << itps << " Z, C: " << zc << ", " << cc << endl;
+    assert( zc );
+    assert( cc );
+    if ( isNorthTps[itps] ) {
+      assert( zc < cc );
+    } else if ( isSouthTps[itps] ) {
+      assert( zc > cc );
+    } else {
+      assert(false);
+    }
+  }
+
+  bool showFembBlocks = show > 1;
+  if ( showFembBlocks ) {
+    cout << myname << line << endl;
+    cout << myname << "Fetching FEMB block ranges." << endl;
+    nbad = 0;
+    IndexVector chk(15360, 0);
+    for ( Index iapa=1; iapa<=napa; ++iapa ) {
+      for ( string view : {"u", "v", "x"} ) {
+        for ( Index ifmb=1; ifmb<=20; ++ifmb ) {
+          ostringstream ssnam;
+          ssnam << "femb" << iapa << setfill('0') << setw(2) << ifmb << view;
+          string nam = ssnam.str();
+          IndexRange ir = irt->get(nam);
+          if ( ! ir.isValid() ) {
+            cout << myname << "Invalid range: " << nam << endl;
+            ++nbad;
+          } else {
+            cout << myname << setw(10) << ir.name << setw(20) << ir.rangeString()
+                 << " " << ir.label();
+            for ( Index ilab=1; ilab<ir.labels.size(); ++ilab ) cout << ", " << ir.label(ilab);
+            for ( Index icha=ir.begin; icha<ir.end; ++icha ) chk[icha] += 1;
+            cout << endl;
+          }
+        }
+      }
+    }
+    for ( Index icha=0; icha<15360; ++icha ) {
+      assert( chk[icha] == 1 );
+    }
+  }
+  assert( nbad == 0 );
+
+  cout << myname << line << endl;
+  cout << "Fetch bad range" << endl;
+  IndexRange irb = irt->get("rangebad");
+  cout << irb.rangeString() << endl;
+  assert( ! irb.isValid() );
+
+  cout << myname << line << endl;
+  cout << myname << "Done." << endl;
+  return 0;
+}
+
+//**********************************************************************
+
+int main(int argc, char* argv[]) {
+  bool useExistingFcl = false;
+  int show = 1;
+  if ( argc > 1 ) {
+    string sarg(argv[1]);
+    if ( sarg == "-h" ) {
+      cout << "Usage: " << argv[0] << " [keepFCL] [SHOW]" << endl;
+      cout << "  If keepFCL = true, existing FCL file is used." << endl;
+      cout << "  SHOW > 1 also shows the 480 FEMB blocks." << endl;
+      return 0;
+    }
+    useExistingFcl = sarg == "true" || sarg == "1";
+  }
+  if ( argc > 2 ) {
+    string sarg(argv[2]);
+    show = std::stoi(sarg);
+  }
+  return test_PdhdChannelRanges(useExistingFcl, show);
+}
+
+//**********************************************************************
