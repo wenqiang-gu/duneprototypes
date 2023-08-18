71b2bf21463b445a884eaf3b98938325439a3779
Jacob Calcutt <calcuttj@msu.edu>
2023-08-18 14:52:00 -0500

    Flushing out DAPHNE reader

--- duneprototypes/Protodune/hd/RawDecoding/DAPHNEReaderPDHD_module.cc
+++ duneprototypes/Protodune/hd/RawDecoding/DAPHNEReaderPDHD_module.cc
@@ -17,7 +17,20 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
+
+
+#include "dunecore/DuneObj/DUNEHDF5FileInfo2.h"
+#include "dunecore/HDF5Utils/HDF5RawFile2Service.h"
+
+//#include "PDHDReadoutUtils.h"
+
+#include "lardataobj/RawData/OpDetWaveform.h"
+#include "lardataobj/RecoBase/OpHit.h"
+
 #include <memory>
+namespace pdhd {
+
+using dunedaq::daqdataformats::SourceID;
 
 class DAPHNEReaderPDHD;
 
@@ -39,35 +52,74 @@ public:
 
 private:
 
-  std::string fInputLabel, fOutputLabel;
+  std::string fInputLabel, fOutputLabel, fFileInfoLabel, fSubDetString;
 
+  bool CheckSourceIsDetector(const SourceID & id);
 };
+}
 
 
-DAPHNEReaderPDHD::DAPHNEReaderPDHD(fhicl::ParameterSet const& p)
+pdhd::DAPHNEReaderPDHD::DAPHNEReaderPDHD(fhicl::ParameterSet const& p)
   : EDProducer{p}, 
-    fInputLabel(p.get<std::string>("InputLabel", "daq"), 
-    fOutputLabel(p.get<std::string>("OutputLabel", "daq")
+    fInputLabel(p.get<std::string>("InputLabel", "daq")),
+    fOutputLabel(p.get<std::string>("OutputLabel", "daq")),
+    fFileInfoLabel(p.get<std::string>("FileInfoLabel", "daq")),
+    fSubDetString(p.get<std::string>("SubDetString","HD_PDS"))
 {
   produces<std::vector<raw::OpDetWaveform>> (fOutputLabel);
   produces<std::vector<recob::OpHit>> (fOutputLabel);
 }
 
 
-void DAPHNEReaderPDHD::produce(art::Event& e)
+bool pdhd::DAPHNEReaderPDHD::CheckSourceIsDetector(const SourceID & id) {
+  return (id.subsystem == SourceID::Subsystem::kDetectorReadout);
+}
+
+void pdhd::DAPHNEReaderPDHD::produce(art::Event& evt)
 {
   std::vector<raw::OpDetWaveform> opdet_waveforms;
   std::vector<recob::OpHit> optical_hits;
 
-  e.put(
+  auto infoHandle = evt.getHandle<raw::DUNEHDF5FileInfo2>(fFileInfoLabel);
+  const std::string & file_name = infoHandle->GetFileName();
+  uint32_t runno = infoHandle->GetRun();
+  size_t   evtno = infoHandle->GetEvent();
+  size_t   seqno = infoHandle->GetSequence();
+
+  dunedaq::hdf5libs::HDF5RawDataFile::record_id_t record_id
+      = std::make_pair(evtno, seqno);
+
+  std::cout << file_name << " " << runno << " " << evtno << " " << seqno <<
+               std::endl;
+
+  art::ServiceHandle<dune::HDF5RawFile2Service> rawFileService;
+  auto raw_file = rawFileService->GetPtr();
+  auto source_ids = raw_file->get_source_ids(record_id);
+
+  for (const auto & source_id : source_ids)  {
+    // only want detector readout data (i.e. not trigger info)
+    if (!CheckSourceIsDetector(source_id)) continue;
+
+    auto geo_ids = raw_file->get_geo_ids_for_source_id(record_id, source_id);
+    for (const auto &geo_id : geo_ids) {
+      //uint16_t det_id = 0xffff & geo_id;
+      dunedaq::detdataformats::DetID::Subdetector det_idenum = static_cast<dunedaq::detdataformats::DetID::Subdetector>(0xffff & geo_id);
+      auto subdetector_string = dunedaq::detdataformats::DetID::subdetector_to_string(det_idenum);
+      if (subdetector_string != fSubDetString) continue;
+
+      std::cout << subdetector_string << std::endl;
+    }
+  }
+
+  evt.put(
       std::make_unique<decltype(opdet_waveforms)>(std::move(opdet_waveforms)),
       fOutputLabel
   );
 
-  e.put(
+  evt.put(
       std::make_unique<decltype(optical_hits)>(std::move(optical_hits)),
       fOutputLabel
   );
 }
 
-DEFINE_ART_MODULE(DAPHNEReaderPDHD)
+DEFINE_ART_MODULE(pdhd::DAPHNEReaderPDHD)
