52f143ee9ae6870c12b04064b95f51ed26664112
Jacob Calcutt <calcuttj@msu.edu>
2023-09-07 11:00:30 -0500

    Adding DAPHNEStreamFrame functionality -- maybe not useful

--- duneprototypes/Protodune/hd/RawDecoding/DAPHNEReaderPDHD_module.cc
+++ duneprototypes/Protodune/hd/RawDecoding/DAPHNEReaderPDHD_module.cc
@@ -20,6 +20,7 @@
 
 
 #include "detdataformats/daphne/DAPHNEFrame.hpp"
+#include "detdataformats/daphne/DAPHNEStreamFrame.hpp"
 #include "dunecore/DuneObj/DUNEHDF5FileInfo2.h"
 #include "dunecore/HDF5Utils/HDF5RawFile2Service.h"
 
@@ -32,6 +33,9 @@
 namespace pdhd {
 
 using dunedaq::daqdataformats::SourceID;
+using dunedaq::daqdataformats::Fragment;
+using DAPHNEStreamFrame = dunedaq::fddetdataformats::DAPHNEStreamFrame;
+using DAPHNEFrame = dunedaq::fddetdataformats::DAPHNEFrame;
 
 class DAPHNEReaderPDHD;
 
@@ -56,6 +60,9 @@ private:
   std::string fInputLabel, fOutputLabel, fFileInfoLabel, fSubDetString;
 
   bool CheckSourceIsDetector(const SourceID & id);
+  template <class T> size_t GetNFrames(size_t frag_size, size_t frag_header_size);
+  void ProcessFrame(std::unique_ptr<Fragment> & frag, size_t frame_size, size_t i, std::vector<raw::OpDetWaveform> & opdet_waveforms);
+  void ProcessStreamFrame(std::unique_ptr<Fragment> & frag, size_t frame_size, size_t i, std::vector<raw::OpDetWaveform> & opdet_waveforms);
 };
 }
 
@@ -76,8 +83,48 @@ bool pdhd::DAPHNEReaderPDHD::CheckSourceIsDetector(const SourceID & id) {
   return (id.subsystem == SourceID::Subsystem::kDetectorReadout);
 }
 
+template<typename T>
+size_t pdhd::DAPHNEReaderPDHD::GetNFrames(size_t frag_size, size_t frag_header_size) {
+  return (frag_size - frag_header_size)/sizeof(T);
+}
+
+
+void pdhd::DAPHNEReaderPDHD::ProcessFrame(
+    std::unique_ptr<Fragment> & frag,
+    size_t frame_size,
+    size_t i,
+    std::vector<raw::OpDetWaveform> & opdet_waveforms) {
+  auto frame
+      = reinterpret_cast<DAPHNEFrame*>(
+          static_cast<uint8_t*>(frag->get_data()) + i*frame_size);
+  raw::OpDetWaveform waveform(frame->get_timestamp(), i, frame->s_num_adcs);
+  for (size_t j = 0; j < static_cast<size_t>(frame->s_num_adcs); ++j) {
+    //std::cout << "\t" << frame->get_adc(j) << std::endl;
+    waveform.push_back(frame->get_adc(j));
+  }
+  opdet_waveforms.emplace_back(waveform);
+}
+
+void pdhd::DAPHNEReaderPDHD::ProcessStreamFrame(
+    std::unique_ptr<Fragment> & frag,
+    size_t frame_size,
+    size_t i,
+    std::vector<raw::OpDetWaveform> & opdet_waveforms) {
+  auto frame
+      = reinterpret_cast<DAPHNEStreamFrame*>(
+          static_cast<uint8_t*>(frag->get_data()) + i*frame_size);
+  //raw::OpDetWaveform waveform(frame->get_timestamp(), i, frame->s_num_adcs);
+  //for (size_t j = 0; j < static_cast<size_t>(frame->s_num_adcs); ++j) {
+    //std::cout << "\t" << frame->get_adc(j) << std::endl;
+    //waveform.push_back(frame->get_adc(j));
+  //}
+  std::cout << frame->header.channel_0 << " " <<
+               frame->header.channel_1 << " " <<
+               frame->header.channel_2 << " " <<
+               frame->header.channel_3 << std::endl;
+  //opdet_waveforms.emplace_back(waveform);
+}
 void pdhd::DAPHNEReaderPDHD::produce(art::Event& evt) {
-  using dunedaq::fddetdataformats::DAPHNEFrame;
   using dunedaq::daqdataformats::FragmentHeader;
 
   std::vector<raw::OpDetWaveform> opdet_waveforms;
@@ -126,27 +173,49 @@ void pdhd::DAPHNEReaderPDHD::produce(art::Event& evt) {
       std::cout << "Getting fragment" << std::endl;
       auto frag = raw_file->get_frag_ptr(record_id, source_id);
       auto frag_size = frag->get_size();
-      size_t frag_header_size = sizeof(FragmentHeader);
+      size_t frag_header_size = sizeof(FragmentHeader); // make this a const class member
 
       // Too small to even have a header
       if (frag_size <= frag_header_size) continue;
 
-      size_t n_frames = (frag_size - frag_header_size)/sizeof(DAPHNEFrame);
+
+      //Checking which type of DAPHNE Frame to use
+      //const bool use_stream_frame = (crate_from_geo == 1);
+      if (crate_from_geo != 2 && crate_from_geo != 1) 
+        throw cet::exception("DAPHNEReaderPDHD")
+          << "Found bad crate number: " << crate_from_geo;
+          
+                                           
+      //size_t n_frames = (frag_size - frag_header_size)/sizeof(DAPHNEFrame);
+      size_t n_frames
+          = (crate_from_geo == 2 ?
+             GetNFrames<DAPHNEFrame>(frag_size, frag_header_size) :
+             GetNFrames<DAPHNEStreamFrame>(frag_size, frag_header_size));
+      auto frame_size = (crate_from_geo == 2 ?
+                         sizeof(DAPHNEFrame) : sizeof(DAPHNEStreamFrame));
       std::cout << "NFrames: " << n_frames << " Headder TS: " <<
                    frag->get_header().trigger_timestamp << std::endl;
       for (size_t i = 0; i < n_frames; ++i) {
-        auto frame = reinterpret_cast<DAPHNEFrame*>(
-            static_cast<uint8_t*>(frag->get_data()) + i*sizeof(DAPHNEFrame));
+        if (crate_from_geo == 2) {
+          ProcessFrame(frag, frame_size, i, opdet_waveforms);
+        }
+        else {
+          ProcessStreamFrame(frag, frame_size, i, opdet_waveforms);
+        }
 
-        std::cout << i << " " << frame->get_channel() << " " <<
-                     frame->get_timestamp() << " " << frame->s_num_adcs <<
-                     std::endl;
-        raw::OpDetWaveform waveform(frame->get_timestamp(), i, frame->s_num_adcs);
+        //std::cout << i << " " <<
+        //             frame->get_timestamp() << " " << frame->s_num_adcs <<
+        //             //" " << frame->daq_header.slot_id <<
+        //             std::endl;
+        //std::cout << frame->daq_header << std::endl;
+        //int channel = frame->get_channel();
+        //std::cout  << "\tChannel: " << channel << " done" << std::endl;
+        /*raw::OpDetWaveform waveform(frame->get_timestamp(), i, frame->s_num_adcs);
         for (size_t j = 0; j < static_cast<size_t>(frame->s_num_adcs); ++j) {
           //std::cout << "\t" << frame->get_adc(j) << std::endl;
           waveform.push_back(frame->get_adc(j));
         }
-        opdet_waveforms.emplace_back(waveform);
+        opdet_waveforms.emplace_back(waveform);*/
       }
     }
   }
